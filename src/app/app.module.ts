d57a2009d9fd1aaa4fda2c86cf541ffa400477b7
bhanuka <bhanukamax@gmail.com>
2017-06-15 21:38:10 +0530

    initial commit, done upto multiple components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailsComponent } from './hero-details.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+	  AppComponent,
+	  HeroDetailsComponent
   ],
   imports: [
-    BrowserModule
+	  BrowserModule,
+	  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
